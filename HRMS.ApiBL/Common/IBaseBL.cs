1bac10db53d4115b5a6b93827e9a2e5efd775bb6
MCBInico <nico234.lds234@gmail.com>
2020-01-28 11:01:18 +0800

    fgh

--- HRMS.ApiBL/Common/IBaseBL.cs
+++ HRMS.ApiBL/Common/IBaseBL.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using HRMS.DL;
 
-namespace HRMS.ApiBL.Common
+namespace HRMS.BL.Common
 {
-    interface IBaseBL
+    public interface IBaseBL<TEntity> where TEntity : class
     {
+        IEnumerable<TEntity> Get();
+        TEntity Get(int id);
+        IEnumerable<TEntity> Search(int offset, int limit, string orderBy);
+        MessageViewDomain Command(TEntity entity, string commandType);
+        MessageViewDomain Delete(int id);
     }
-}
+
+    public class BaseBL
+    {
+
+        protected MessageViewDomain GetMessage(DataTable dataTable)
+        {
+            return dataTable.AsEnumerable()
+                .Select(dataRow => new MessageViewDomain
+                {
+                    DataID = dataRow.Field<int>("DataID"),
+                    Message = dataRow.Field<string>("Message"),
+                    RetVal = dataRow.Field<int>("RetVal")
+                }).FirstOrDefault();
+        }
+    }
